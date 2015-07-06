e366de4fe5e8d87e5a6541c2c4fdc0d0e8a9039b
admin <admin@admindeMac-mini.local>
2015-07-06 09:18:19 +0800

    Initial Commit

---
+++ lyyTests/lyyTests.swift
@@ -0,0 +1,36 @@
+//
+//  lyyTests.swift
+//  lyyTests
+//
+//  Created by admin on 15/7/6.
+//  Copyright (c) 2015年 lyy. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lyyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
