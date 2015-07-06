99a14e84a63080affd908f3bbbbbae2dffd965f5
admin <admin@admindeMac-mini.local>
2015-07-06 15:01:06 +0800

    初始版本补充

--- lyy/ViewController.swift
+++ lyy/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cehua(sender: AnyObject) {
+        println("============================")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
